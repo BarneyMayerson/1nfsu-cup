c0522e46a63431cc426ab9b9ce1d5986036f81df
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-02-18 16:51:49 +0300

    Vue plugin for vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import "./bootstrap";
+import { createApp } from "vue/dist/vue.esm-bundler";
+
+import Hello from "./components/Hello.vue";
+
+createApp({
+  components: {
+    Hello,
+  },
+}).mount("#app");
---
+++ resources/js/components/Hello.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+  name: String,
+});
+</script>
+
+<template>
+  <div class="inline-block">
+    <h2 class="text-2xl font-semibold">Hello {{ name }}</h2>
+  </div>
+</template>
