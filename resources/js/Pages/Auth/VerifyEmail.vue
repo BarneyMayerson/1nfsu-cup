423b590aa4fcc0d2152814a705db06c6babb6ae8
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-02-20 12:16:55 +0300

    Email verification (backend)

---
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+  <Head title="Verify Email" />
+  <div class="flex justify-center py-6">Verify email page goes here...</div>
+</template>
--- routes/auth.php
+++ routes/auth.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("guest")->group(function () {
@@ -18,6 +20,13 @@ Route::middleware("guest")->group(function () {
 });
 
 Route::middleware("auth")->group(function () {
+    Route::get("verify-email", EmailVerificationPromptController::class)->name(
+        "verification.notice"
+    );
+    Route::get("verify-email/{id}/{hash}", VerifyEmailController::class)
+        ->middleware(["signed", "throttle:6,1"])
+        ->name("verification.verify");
+
     Route::post("logout", [
         AuthenticatedSessionController::class,
         "destroy",
