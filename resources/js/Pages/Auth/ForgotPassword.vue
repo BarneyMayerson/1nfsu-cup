aa24f507efbe156e62c7684f73501a24328451d8
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-02-21 16:16:56 +0300

    Reset password (backend)

---
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+  <Head title="Forgot Password" />
+  <div class="flex justify-center py-6">Forgot Password page goes here...</div>
+</template>
---
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+
+defineProps({
+  email: String,
+  token: String,
+});
+</script>
+
+<template>
+  <Head title="Reset Password" />
+  <div class="flex justify-center py-6">
+    <p>Reset password page goes here...</p>
+    <div class="mt-6 block">email = {{ email }}</div>
+    <div class="mt-6 block">token = {{ token }}</div>
+  </div>
+</template>
