3759a148a27af7b6eda880e674cc685ccd0adac1
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-03-03 15:38:25 +0300

    NFSU brand trivia SpinCircle component

---
+++ resources/js/Components/NFSUBrandTrivia/SpinCircle.vue
@@ -0,0 +1,74 @@
+<script setup>
+defineProps({
+  pulseDuration: {
+    type: Number,
+    default: 2,
+  },
+  rotateDuration: {
+    type: Number,
+    default: 2,
+  },
+  opacityValues: {
+    type: String,
+    default: "0;1;0",
+  },
+});
+</script>
+<template>
+  <svg :class="`h-14 w-14 rotate-${initiatRotate}`">
+    <circle
+      class="text-gray-300 dark:text-gray-500"
+      fill="transparent"
+      stroke-width="6"
+      stroke="currentColor"
+      r="21"
+      cx="28"
+      cy="28"
+      stroke-dasharray="29,4"
+    />
+    <animate
+      attributeName="stroke-dashoffset"
+      :dur="rotateDuration"
+      autoplay="true"
+      repeatCount="indefinite"
+      values="0;264"
+    />
+
+    <animate
+      attributeName="opacity"
+      :values="opacityValues"
+      :dur="pulseDuration"
+      repeatCount="indefinite"
+    />
+  </svg>
+</template>
+
+<style scoped>
+.animate-fade {
+  -webkit-animation: fade 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+  animation: fade 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+}
+
+@-webkit-keyframes fade {
+  0% {
+    opacity: 0.15;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0.15;
+  }
+}
+@keyframes fade {
+  0% {
+    opacity: 0.15;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0.15;
+  }
+}
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head } from "@inertiajs/vue3";
 import LikeGameMenuItem from "@/Navigations/LikeGameMenuItem.vue";
+import SpinCircle from "@/Components/NFSUBrandTrivia/SpinCircle.vue";
 </script>
 
 <script>
@@ -13,11 +14,25 @@ export default {
 
 <template>
   <Head title="Home" />
-  <main class="container mx-auto">
+  <main class="rota container mx-auto">
     <div class="mt-8 px-4">Main content goes here...</div>
+
+    <div class="mt-8 flex items-center justify-start">
+      <SpinCircle :pulseDuration="3" :rotateDuration="3" />
+      <SpinCircle :pulseDuration="1.3" opacityValues="0.2;1;0.2" />
+      <SpinCircle
+        :pulseDuration="1.8"
+        :rotateDuration="2.7"
+        opacityValues="0.5;0.9;0.5"
+      />
+      <SpinCircle :rotateDuration="2.8" opacityValues="0.3;1;0.3" />
+    </div>
 
     <div class="mt-8">
       <LikeGameMenuItem>Tourneys</LikeGameMenuItem>
+      <LikeGameMenuItem>Competitions</LikeGameMenuItem>
+      <LikeGameMenuItem>Standings</LikeGameMenuItem>
+      <LikeGameMenuItem>Game Server</LikeGameMenuItem>
     </div>
 
     <div class="mt-8 grid h-96 place-items-center border border-green-300">
