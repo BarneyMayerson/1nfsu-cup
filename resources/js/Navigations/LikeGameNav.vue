06872937acd3662f14a1f9b12b16002172a3b270
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-03-04 08:35:22 +0300

    Extract LakeGameNav component for the hero page

---
+++ resources/js/Navigations/LikeGameNav.vue
@@ -0,0 +1,32 @@
+<script setup>
+import LikeGameMenuItem from "@/Navigations/LikeGameMenuItem.vue";
+import SpinCircle from "@/Components/NFSUBrandTrivia/SpinCircle.vue";
+</script>
+
+<template>
+  <div>
+    <div class="flex items-center justify-end">
+      <SpinCircle :pulseDuration="3" :rotateDuration="3" />
+      <SpinCircle :pulseDuration="1.3" opacityValues="0.2;1;0.2" />
+      <SpinCircle
+        :pulseDuration="1.8"
+        :rotateDuration="2.7"
+        opacityValues="0.5;0.9;0.5"
+      />
+      <SpinCircle :rotateDuration="2.8" opacityValues="0.3;1;0.3" />
+    </div>
+    <div class="my-3 flex justify-end">
+      <h3
+        class="font-sans text-4xl uppercase italic tracking-wider text-sky-500 dark:text-sky-300"
+      >
+        Main Menu
+      </h3>
+    </div>
+    <nav class="space-y-2">
+      <LikeGameMenuItem>Tourneys</LikeGameMenuItem>
+      <LikeGameMenuItem>Competitions</LikeGameMenuItem>
+      <LikeGameMenuItem>Standings</LikeGameMenuItem>
+      <LikeGameMenuItem>Game Server</LikeGameMenuItem>
+    </nav>
+  </div>
+</template>
