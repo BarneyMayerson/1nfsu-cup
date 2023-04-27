acc86c17510800ff82922fae8385c4787535d9ce
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-04-27 15:31:53 +0300

    Fixing disclosure closing (deleting previously extracted menus)

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -2,15 +2,17 @@
 import { computed } from "vue";
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import { usePage } from "@inertiajs/vue3";
+import { route } from "momentum-trail";
+import GenericMenuItem from "@/Components/Nav/GenericMenuItem.vue";
 import LogoSubmenu from "@/Navigations/LogoSubmenu.vue";
 import MainNav from "@/Navigations/MainNav.vue";
 import AuthUserDropdownMenu from "@/Navigations/AuthUserDropdownMenu.vue";
 import GuestSubmenu from "@/Navigations/GuestSubmenu.vue";
-import AuthMobileMenu from "@/Navigations/AuthMobileMenu.vue";
-import GuestMobileMenu from "@/Navigations/GuestMobileMenu.vue";
 import ToggleMode from "@/Components/ToggleMode.vue";
 import XMarkIcon from "@/Components/Shared/Icons/XMarkIcon.vue";
 import Bars3Icon from "@/Components/Shared/Icons/Bars3Icon.vue";
+import BellIcon from "@/Components/Shared/Icons/BellIcon.vue";
+import Avatar from "@/Components/User/Avatar.vue";
 
 const currentUser = computed(() => usePage().props.auth.user);
 const isAuth = computed(() => Boolean(currentUser.value));
@@ -55,13 +57,87 @@ const isHome = computed(() => usePage().props.isHome);
           </div>
         </div>
 
-        <DisclosurePanel class="space-y-4 divide-y rounded-lg border py-4">
-          <MainNav :vertical="true" class="px-4" />
+        <DisclosurePanel
+          class="space-y-4 divide-y rounded-lg border py-4"
+          v-slot="{ close }"
+        >
+          <nav aria-label="Site Nav" class="px-4">
+            <ul class="flex flex-col items-start gap-3">
+              <li>
+                <GenericMenuItem
+                  @click="close()"
+                  :href="route('competitions.index')"
+                >
+                  Competitions
+                </GenericMenuItem>
+              </li>
+              <li>
+                <GenericMenuItem
+                  @click="close()"
+                  :href="route('tourneys.index')"
+                >
+                  Tourneys
+                </GenericMenuItem>
+              </li>
+              <li>
+                <GenericMenuItem
+                  @click="close()"
+                  :href="route('standings.index')"
+                >
+                  Standings
+                </GenericMenuItem>
+              </li>
+              <li>
+                <GenericMenuItem
+                  @click="close()"
+                  :href="route('game-server.index')"
+                >
+                  Game Server
+                </GenericMenuItem>
+              </li>
+            </ul>
+          </nav>
+
           <div v-if="isAuth" class="px-4">
-            <AuthMobileMenu />
+            <div class="mt-4 flex items-center">
+              <div class="h-10 w-10 flex-shrink-0">
+                <Avatar :src="currentUser.avatar" />
+              </div>
+              <div class="ml-4">
+                <div class="text-base font-medium">Tom Cook</div>
+                <div class="text-sm font-medium">tom@example.com</div>
+              </div>
+              <button
+                type="button"
+                class="ml-auto flex-shrink-0 rounded-full p-1 text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-200"
+              >
+                <span class="sr-only">View notifications</span>
+                <BellIcon class="h-8 w-8" aria-hidden="true" />
+              </button>
+            </div>
+            <div class="mt-3 flex flex-col items-start gap-2">
+              <GenericMenuItem @click="close()" href="#"
+                >Your Profile</GenericMenuItem
+              >
+              <GenericMenuItem @click="close()" href="#"
+                >Settings</GenericMenuItem
+              >
+              <GenericMenuItem
+                @click="close()"
+                :href="route('logout')"
+                method="post"
+                as="button"
+              >
+                Logout
+              </GenericMenuItem>
+            </div>
           </div>
           <div v-else class="px-4">
-            <GuestMobileMenu />
+            <div class="mt-4">
+              <GenericMenuItem @click="close()" :href="route('register')">
+                Register
+              </GenericMenuItem>
+            </div>
           </div>
         </DisclosurePanel>
       </Disclosure>
