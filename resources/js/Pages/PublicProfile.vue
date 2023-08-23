96311e81345fe9d8b85fcc46ee2bff856ca42516
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-08-23 11:27:12 +0300

    Public Profile finished

--- resources/js/Pages/PublicProfile.vue
+++ resources/js/Pages/PublicProfile.vue
@@ -1,7 +1,18 @@
 <script setup>
 import { Head } from "@inertiajs/vue3";
+import Avatar from "@/Components/User/Avatar.vue";
+import { flagSrc } from "@/Countries/flag";
+import { countryListAlpha2Array } from "@/Countries/countries-list";
 
-defineProps(["user"]);
+const props = defineProps(["user"]);
+
+const mergedName = props.user ? props.user.name.replace(" ", "") : null;
+const country = props.user
+  ? countryListAlpha2Array.find(
+      (country) => country.code === props.user.country
+    ).name
+  : null;
+const flag = props.user ? flagSrc(props.user.country) : null;
 </script>
 
 <template>
@@ -9,10 +20,55 @@ defineProps(["user"]);
   <div class="container mx-auto">
     <main>
       <div v-if="user">
-        <p>Public Profile Goes Here ...</p>
-        <p>{{ user.name }}</p>
-        <p>{{ user.country }}</p>
-        <p>{{ user.avatar }}</p>
+        <div class="grid grid-cols-1 gap-8 md:grid-cols-2">
+          <div
+            class="flex items-start justify-center space-x-4 md:justify-start"
+          >
+            <div
+              class="h-24 w-24 flex-shrink-0 lg:h-36 lg:w-36 xl:h-48 xl:w-48"
+            >
+              <Avatar :src="user.avatar" />
+            </div>
+
+            <div>
+              <h3 class="text-2xl font-semibold tracking-wide">
+                {{ user.name }}
+              </h3>
+              <p class="opacity-50">{{ mergedName }}</p>
+            </div>
+
+            <div>
+              <img id="flag" class="w-32" :src="flag" />
+              <p class="mt-1 text-sm opacity-75">{{ country }}</p>
+            </div>
+          </div>
+
+          <div
+            class="flex items-center justify-center space-x-4 md:justify-end"
+          >
+            <div
+              class="mx-3 h-32 w-40 border-2 bg-transparent p-4 text-center dark:border-sky-400"
+            >
+              <p class="text-lg font-bold">Tourneys</p>
+              <span class="text-6xl">8</span>
+              <span class="text-4xl"> / </span>
+              <span class="text-2xl">1</span>
+            </div>
+
+            <div
+              class="mx-3 h-32 w-40 border-2 bg-transparent p-4 text-center dark:border-sky-400"
+            >
+              <p class="text-lg font-bold">Competitions</p>
+              <span class="text-6xl">18</span>
+              <span class="text-4xl"> / </span>
+              <span class="text-2xl">6</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-4 md:mt-10">
+          <div class="text-xl">Site Points (SP): 123</div>
+        </div>
       </div>
       <div v-else>
         <div class="flex justify-center">
--- tests/Unit/Pages/PublicProfile.test.js
+++ tests/Unit/Pages/PublicProfile.test.js
@@ -1,25 +1,49 @@
-import { describe, it, expect } from "vitest";
-import { mount } from "@vue/test-utils";
+import { describe, it, expect, beforeEach } from "vitest";
+import { shallowMount } from "@vue/test-utils";
 import PublicProfile from "@/Pages/PublicProfile.vue";
+import Avatar from "@/Components/User/Avatar.vue";
+import {
+  countryListAlpha2Array,
+  countryListAlpha2,
+} from "@/Countries/countries-list";
 
 describe("PublicProfile", () => {
-  it("displays the user profile info", () => {
-    const user = {
-      name: "Joe Silver",
-      avatar: "path-to-profile-avatar",
-      country: "BE",
-    };
+  let wrapper = null;
 
-    console.log(user);
-    const wrapper = mount(PublicProfile, {
-      shallow: true,
+  const user = {
+    name: "Joe Silver",
+    country: "BE",
+  };
+
+  beforeEach(() => {
+    wrapper = shallowMount(PublicProfile, {
       props: {
         user,
       },
     });
+  });
 
+  it("displays a user avatar", () => {
+    expect(wrapper.findComponent(Avatar).exists()).toBeTruthy();
+  });
+
+  it("displays the user name", () => {
     expect(wrapper.text()).toContain(user.name);
-    expect(wrapper.text()).toContain(user.avatar);
-    expect(wrapper.text()).toContain(user.country);
+  });
+
+  it("contains an img element with country code", () => {
+    const img = wrapper.find("#flag");
+
+    expect(img.attributes().src).toContain(user.country.toLowerCase());
+  });
+
+  it("contains a country name", () => {
+    const countryName = countryListAlpha2Array.find(
+      (country) => country.code === user.country
+    ).name;
+
+    console.log(countryListAlpha2[user.country]); // sic!
+
+    expect(wrapper.text()).toContain(countryName);
   });
 });
