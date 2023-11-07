84ada1afe7c932fffbe18b64bc979eda3cdec985
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-11-07 11:04:09 +0300

    Updating Profile.test.js

--- tests/Unit/Pages/User/Settings/Profile.test.js
+++ tests/Unit/Pages/User/Settings/Profile.test.js
@@ -2,6 +2,7 @@ import { describe, it, expect, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
 import Profile from "@/Pages/User/Settings/Profile.vue";
 import ProfileForm from "@/Forms/User/Settings/ProfileForm.vue";
+import AvatarForm from "@/Forms/User/Settings/AvatarForm.vue";
 
 describe("Profile.vue", () => {
   let wrapper = null;
@@ -18,6 +19,12 @@ describe("Profile.vue", () => {
 
   it("contains Profile Form", () => {
     const form = wrapper.findComponent(ProfileForm);
+
+    expect(form.exists()).toBeTruthy();
+  });
+
+  it("contains Avatar Form", () => {
+    const form = wrapper.findComponent(AvatarForm);
 
     expect(form.exists()).toBeTruthy();
   });
